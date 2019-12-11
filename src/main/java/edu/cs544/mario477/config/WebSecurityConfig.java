1a6a10baec2952664d6e9de61f019e4d271c5317
Cong Toan Doan <toandc1152@gmail.com>
2019-12-11 10:44:14 -0600

    Register user

---
+++ src/main/java/edu/cs544/mario477/config/WebSecurityConfig.java
@@ -0,0 +1,77 @@
+package edu.cs544.mario477.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    private JwtUserDetailsService jwtUserDetailsService;
+//
+//    @Autowired
+//    private JwtAuthenticationEntryPoint unauthorizedHandler;
+//
+//    @Autowired
+//    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .userDetailsService(jwtUserDetailsService)
+//                .passwordEncoder(passwordEncoder());
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .and()
+//                .authenticationEntryPoint(unauthorizedHandler).and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                .antMatchers("/login", "/register").permitAll()
+                .anyRequest().authenticated();
+
+        // Add our custom JWT security filter
+//        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/**",
+                        "/swagger-ui.html",
+                        "/webjars/**"
+                );
+    }
+}
---
+++ src/main/java/edu/cs544/mario477/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package edu.cs544.mario477.repository;
+
+import edu.cs544.mario477.base.BaseRepository;
+import edu.cs544.mario477.domain.User;
+
+public interface UserRepository extends BaseRepository<User, Long> {
+
+}
