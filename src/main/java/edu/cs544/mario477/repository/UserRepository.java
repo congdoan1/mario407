4f4305d70204c8fd0c0bb4d9280beac99c11d358
pdthong <luis.thong.phan@gmail.com>
2019-12-13 17:17:54 -0600

    no message

--- src/main/java/edu/cs544/mario477/repository/UserRepository.java
+++ src/main/java/edu/cs544/mario477/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package edu.cs544.mario477.repository;
 
 import edu.cs544.mario477.base.BaseRepository;
+import edu.cs544.mario477.domain.Address;
 import edu.cs544.mario477.domain.User;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface UserRepository extends BaseRepository<User, Long> {
@@ -15,4 +17,8 @@ public interface UserRepository extends BaseRepository<User, Long> {
     @Query("select distinct u from User u join u.posts p where " +
             "( select count (p) from p where p.text in (select k.definition from Keyword k where k.enabled=true)) >=20")
     List<User> findMaliciousUser();
+
+    List<User> findUserByAddressIsContaining(Address address);
+
+    List<User> findUserByBirthdayBefore(LocalDate date);
 }
