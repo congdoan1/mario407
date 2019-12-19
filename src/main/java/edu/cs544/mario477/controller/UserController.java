39f302fe4b84fa67f87407a646fcd6f1d29546de
pdthong <luis.thong.phan@gmail.com>
2019-12-19 11:03:24 -0600

    Advertisement update

--- src/main/java/edu/cs544/mario477/controller/UserController.java
+++ src/main/java/edu/cs544/mario477/controller/UserController.java
@@ -2,18 +2,16 @@ package edu.cs544.mario477.controller;
 
 import edu.cs544.mario477.common.Response;
 import edu.cs544.mario477.common.ResponseBuilder;
+import edu.cs544.mario477.domain.Advertisement;
 import edu.cs544.mario477.domain.User;
+import edu.cs544.mario477.dto.AdvertisementDTO;
 import edu.cs544.mario477.dto.UserDTO;
-import edu.cs544.mario477.service.AdminService;
-import edu.cs544.mario477.service.IAuthenticationFacade;
-import edu.cs544.mario477.service.PostService;
-import edu.cs544.mario477.service.UserService;
+import edu.cs544.mario477.service.*;
 import edu.cs544.mario477.util.Mapper;
 import edu.cs544.mario477.util.PageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,16 +28,19 @@ public class UserController {
     private UserService userService;
     private PostService postService;
     private AdminService adminService;
+    private AdvertisementService advertisementService;
 
     @Autowired
+
     public UserController(UserService userService,
                           PostService postService,
                           IAuthenticationFacade authenticationFacade,
-                          AdminService adminService) {
+                          AdminService adminService, AdvertisementService advertisementService) {
         this.userService = userService;
         this.postService = postService;
         this.authenticationFacade = authenticationFacade;
         this.adminService = adminService;
+        this.advertisementService = advertisementService;
     }
 
     @PostMapping("/{username}/follow")
@@ -138,7 +139,6 @@ public class UserController {
         return ResponseBuilder.buildSuccess();
     }
 
-
     @GetMapping("/claims")
     public Response claimUsers(@RequestParam(value = "page", required = false) Integer page,
                                @RequestParam(value = "size", required = false) Integer size) {
@@ -147,4 +147,12 @@ public class UserController {
 
     }
 
+    @GetMapping("/{id}/ads")
+    public Response advertisements(@PathVariable Long id,
+                                   @RequestParam(value = "page", required = false) Integer page,
+                                   @RequestParam(value = "size", required = false) Integer size) {
+        List<AdvertisementDTO> ads = advertisementService.findAdvertisementMatchWithUser(id);
+        return ResponseBuilder.buildSuccess(ads);
+
+    }
 }
